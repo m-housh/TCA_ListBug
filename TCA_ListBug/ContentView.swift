308384df8dd5b340bf1154dd3e8fb6d88767f0ee
Michael Housh <mhoush@houshhomeenergy.com>
2020-10-02 17:44:38 -0400

    Initial Commit

---
+++ TCA_ListBug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCA_ListBug
+//
+//  Created by Michael on 10/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCA_ListBug/TCA_ListBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_ListBugApp.swift
+//  TCA_ListBug
+//
+//  Created by Michael on 10/2/20.
+//
+
+import SwiftUI
+
+@main
+struct TCA_ListBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
